fb309614e02ab478af4d533e879ccfc9c7a9a9b7
Ozan Yücel <ozanyucel249@gmail.com>
2022-05-03 16:09:26 +0300

    added some ui features which are using dialog

--- diary.sh
+++ diary.sh
@@ -2,45 +2,82 @@
 
 # THIS SCRIPT IS BEING DEVELOPED BY OZAN YUCEL | github.com/ozanyucell
 
-while(true) { # not sure if this one is working or not
-    echo "1. Enter dairy for $(date +%D)."
-    echo "2. View or edit an old diary."
-    echo "3. Exit."
+function menu() {
+    CHOICE=$(dialog --menu "Welcome $USER" 12 45 25 1 "Enter dairy for $(date +%D)." 2 "View or edit an old diary." 3 "Exit."\
+        3>&1 1>&2 2>&3 3>&- \ 
+        # if you are curious about what is happening on the line above, check here: https://stackoverflow.com/questions/29222633/bash-dialog-input-in-a-variable
+    )
+}
 
-    read choice
+function sub_menu() {
+    CHOICE=$(dialog --menu "Welcome $USER" 12 45 25 1 "Edit this diary." 2 "Exit."\
+        3>&1 1>&2 2>&3 3>&- \ 
+    )
+}
 
-    if (( $choice == 1 )) # what if user tries to crate a diary for twice at the same day?
+function passwordbox() {   # --passwordbox <text> <height> <width> [<init>]
+    PASSWORD=$(dialog --passwordbox "Password" 10 20\
+        3>&1 1>&2 2>&3 3>&- \
+    )
+}
+
+function password_encryption() { # passwords is in $PASSWORD
+
+}
+
+function text_encryption() { # Diary inputs are in $DIARY_INPUT
+    
+}
+
+function calendar() {
+    DATE=$(dialog --calendar "Calendar" 5 50 $(date +%d) $(date +%m) $(date +%Y)\
+        3>&1 1>&2 2>&3 3>&- \
+    )
+}
+
+function infobox() {
+    dialog  --infobox "$DATE" 15 30   # --infobox <text> <height> <width>
+}
+
+function inputbox() {   # --inputbox <text> <height> <width> [<init>]
+    DIARY_INPUT=$(\
+        dialog \
+        --inputbox "Diary for $DATE_INPUT" 30 50 "$DIARY_INPUT" \
+        3>&1 1>&2 2>&3 3>&- \
+    )
+}
+
+while true 
+do
+    menu
+
+    if (( $CHOICE == 1 )) # what if user tries to crate a diary for twice at the same day?
     then
-        read daily_text
-        echo "Enter a password for this diary."
-        read password
-        password = encryption(password)
-        echo password > $HOME/dairy/texts/$(date +%D)_$USER.dairy
-        echo daily_text >> $HOME/dairy/texts/$(date +%D)_$USER.dairy
+        inputbox
 
-    else if (( $choice == 2 ))
+        passwordbox
+        #take password and input into variables here
+
+        password_encryption
+        text_encryption
+
+        echo password > $HOME/diary/texts/$(date +%D)_$USER.dairy
+        echo daily_text >> $HOME/diary/texts/$(date +%D)_$USER.dairy # here needs to be encrypted
+
+    else if (( $CHOICE == 2 ))
     then
         echo "Please enter the password."
         read password
         password = encryption(password)
 
-        echo "Please enter the year"
-        read year
+        calendar
 
-        echo "Please enter the month"
-        read month
+        cat $HOME/diary/texts/$DATE_$USER.diary # not sure about this pattern, date returns dd/mm/yy. it will collide with path's pattern
 
-        echo "Please enter the day"
-        read day
+        sub_menu # MISSING CODE HERE
 
-        cat $HOME/diary/texts/$year_$month_$day_$USER.diary # not sure about this pattern
-
-        echo "1. Edit this diary."
-        echo "2. Exit."
-        read selection
-
-        if (( $selection == 1 ))
-        then
+        if (( $CHOICE == 1 ))
+        then # NEEDS ENCRYPTION
             echo password > $HOME/diary/texts/$year_$month_$day_$USER.diary
             read new_text
             echo new_text >> $HOME/diary/texts/$year_$month_$day_$USER.diary
@@ -51,24 +88,4 @@ while(true) { # not sure if this one is working or not
     else
         exit()
     fi
-}
-
-function password() {
-    return dialog --passwordbox "Password" 10 20   # --passwordbox <text> <height> <width> [<init>]
-}
-
-function encryption(password) {
-
-}
-
-function calendar() {
-
-}
-
-function infobox(Date) {
-    return dialog  --infobox "$Date" 15 30   # --infobox <text> <height> <width>
-}
-
-function inputbox(Date, init) {
-    return dialog  --inputbox "$Date" 15 30 "$init"   # --inputbox <text> <height> <width> [<init>]
-}
+done
