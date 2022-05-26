51573a89fdffae2ab65479e942e35aeb2fd32c0c
Ozan Yücel <ozanyucel249@gmail.com>
2022-05-26 16:37:51 +0300

    this one needs to be tested

--- diary.sh
+++ diary.sh
@@ -3,7 +3,7 @@
 # THIS SCRIPT IS BEING DEVELOPED BY OZAN YUCEL & MERT YILDIZ | github.com/ozanyucell & github.com/myildiz21
 
 function menu() {
-    CHOICE=$(dialog --menu "Welcome $USER" 12 45 25 1 "Enter dairy for $(date +%D)." 2 "View or edit an old diary." 3 "Exit."\
+    CHOICE=$(dialog --menu "Welcome $USER" 12 45 25 1 "Enter dairy for $(date +%D)." 2 "View an old diary." 3 "Exit."\
         3>&1 1>&2 2>&3 3>&- \ 
         # if you are curious about what is happening on the line above, check here: https://stackoverflow.com/questions/29222633/bash-dialog-input-in-a-variable
     )
@@ -28,7 +28,7 @@ function calendar() {
 }
 
 function infobox() {
-    dialog  --infobox "$DATE" 15 30   # --infobox <text> <height> <width>
+    dialog  --infobox "$TEXT" 15 30   # --infobox <text> <height> <width>
 }
 
 function inputbox() {   # --inputbox <text> <height> <width> [<init>]
@@ -40,11 +40,11 @@ function inputbox() {   # --inputbox <text> <height> <width> [<init>]
 }
 
 function file_to_zip() {  # for locking the diary https://www.tecmint.com/create-password-protected-zip-file-in-linux/
-    zip -p $PASSWORD $FILE_PATH.zip $FILE_PATH
+    zip -p $PASSWORD $FILE_PATH.zip $FILE_PATH.diary
 }
 
 function zip_to_file() {  # for unlocking the diary https://www.shellhacks.com/create-password-protected-zip-file-linux/
-    unzip -p $PASSWORD $FILE_PATH.zip
+    unzip -p $PASSWORD $ZIP_PATH
 }
 
 
@@ -52,42 +52,49 @@ while true
 do
     menu
 
-    if (( $CHOICE == 1 )) # what if user tries to crate a diary for twice at the same day?
+    if (( $CHOICE == 1 )) # Enter diary for current date
+    # what if user tries to crate a diary for twice at the same day?
     then
         # takes password and input into variables here
         inputbox
         passwordbox
 
-        FILE_PATH=$HOME/diary/texts/$(date +%d)-$(date +%m)-$(date +%Y)-$USER.dairy 
+        FILE_PATH=$HOME/diary/texts/$(date +%d)-$(date +%m)-$(date +%Y)-$USER
 
-        echo DIARY_INPUT >> $FILE_PATH # here needs to be encrypted
+        echo $DIARY_INPUT >> $FILE_PATH.diary # here needs to be encrypted
         
         # converts file into zip with password
-        zip_converter # METHOD IS EMPTY !!
+        file_to_zip
 
         # deletes the unprotected diary file
-        rm $FILE_PATH
+        rm $FILE_PATH.diary
 
-    else if (( $CHOICE == 2 ))
+    elif (( $CHOICE == 2 )) # view an old diary
     then
         # pick date and password
         calendar
         passwordbox
 
-        FILE_PATH=$HOME/diary/texts/$DATE-$USER.diary # not sure about this pattern, date returns dd/mm/yy. it will collide with path's pattern
+        # unlock zip with the password taken and extract
+        ZIP_PATH=$HOME/diary/texts/$DATE-$USER.zip
+        zip_to_file
 
-        sub_menu # MISSING CODE HERE
+        # save the content into a variable
+        FILE_PATH=$HOME/diary/texts/$DATE-$USER.diary
+        TEXT=$(cat $FILE_PATH)
+        
+        # print content to screen
+        infobox
 
-        if (( $CHOICE == 1 ))
-        then # NEEDS ENCRYPTION
-            echo password > $HOME/diary/texts/$year-$month-$day-$USER.diary
-            read new_text
-            echo new_text >> $HOME/diary/texts/$year-$month-$day-$USER.diary
-        else
-            continue
-        fi
+        # remove unsecured file
+        rm $FILE_PATH
+
+    elif (( $CHOICE == 3 )) # exit
+        exit() # exit the script
 
     else
-        exit()
+        TEXT="Please enter a valid option."
+        infobox
+        continue
     fi
 done
