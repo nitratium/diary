bd937b613488f09a171102fdb026ec026dae0517
Ozan Yücel <68647366+ozanyucell@users.noreply.github.com>
2022-05-29 15:33:20 +0300

    there is a problem with else condition, I dunno why
    everything else works properly

--- diary.sh
+++ diary.sh
@@ -7,22 +7,26 @@ function menu() {
         3>&1 1>&2 2>&3 3>&- \ 
         # if you are curious about what is happening on the line above, check here: https://stackoverflow.com/questions/29222633/bash-dialog-input-in-a-variable
     )
+    clear
 }
 
 function passwordbox() {   # --passwordbox <text> <height> <width> [<init>]
     PASSWORD=$(dialog --passwordbox "Password" 10 20\
         3>&1 1>&2 2>&3 3>&- \
     )
+    clear
 }
 
 function calendar() {
-    DATE=$(dialog --calendar "Calendar" 5 50 $(date +%d) $(date +%m) $(date +%Y)\
+    DATE=$(dialog --calendar "Calendar" 5 50 $(date +%d) $(date +%m) $(date +%Y)\  # returns the date as dd/mm/yyyy, which will collide with the path. we need to handle this
         3>&1 1>&2 2>&3 3>&- \
     )
+    clear
 }
 
 function infobox() {
     dialog  --infobox "$TEXT" 15 30   # --infobox <text> <height> <width>
+    clear # not sure if we need to use clear here. probably yes
 }
 
 function inputbox() {   # --inputbox <text> <height> <width> [<init>]
@@ -31,6 +35,7 @@ function inputbox() {   # --inputbox <text> <height> <width> [<init>]
         --inputbox "Diary for $DATE_INPUT" 30 50 "$DIARY_INPUT" \
         3>&1 1>&2 2>&3 3>&- \
     )
+    clear
 }
 
 function file_to_zip() {  # for locking the diary https://www.tecmint.com/create-password-protected-zip-file-in-linux/
@@ -84,6 +89,7 @@ do
         rm $FILE_PATH
 
     elif (( $CHOICE == 3 )) # exit
+    then
         exit() # exit the script
 
     else
